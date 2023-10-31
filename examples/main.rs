6dbe3a43a1997db2bfc90d8d71533f952d42c352
gw31415 <git@amas.dev>
2023-11-01 03:58:24 +0900

    use thread_local for auto initialization

--- examples/main.rs
+++ examples/main.rs
@@ -1,10 +1,9 @@
-use mathjax_svg::{Converter, Error};
+use mathjax_svg::{convert_to_svg, Error};
 
 fn main() -> Result<(), Error> {
-    let mut converter = Converter::new();
     println!(
         "{}",
-        converter.convert_to_svg(r#"\int_{-\infty}^\infty e^{-x^2}\,\mathrm dx"#)?
+        convert_to_svg(r"\int_{-\infty}^\infty e^{-x^2}\,\mathrm dx")?
     );
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::sync::Mutex;
+use std::{cell::RefCell, sync::Mutex};
 
 use thiserror::Error;
 use v8::{Context, Global, OwnedIsolate};
@@ -37,98 +37,89 @@ pub enum Error {
 /// local shortcode of Result
 type Result<T> = std::result::Result<T, Error>;
 
-/// Math to Svg Converter
-pub struct Converter {
-    isolate: OwnedIsolate,
-    context: Global<Context>,
-}
-
 const FUNC_ID: &str = "cqbuwfsowtpq"; // random character string
 
-impl Default for Converter {
-    fn default() -> Self {
-        Self::new()
+/// Initialization: JIT compilation and function object registration
+fn initialize() -> (RefCell<OwnedIsolate>, Global<Context>) {
+    if !*INITIALIZED.lock().unwrap() {
+        let platform = v8::new_default_platform(0, false).make_shared();
+        v8::V8::initialize_platform(platform);
+        v8::V8::initialize();
+        *INITIALIZED.lock().unwrap() = true;
     }
+
+    let mut isolate = v8::Isolate::new(Default::default());
+    let global_context;
+    {
+        let handle_scope = &mut v8::HandleScope::new(&mut isolate);
+        let context = v8::Context::new(handle_scope);
+        global_context = Global::new(handle_scope, context);
+        {
+            // JIT compilation and function object registration
+
+            // Generate scope
+            let context = v8::Local::new(handle_scope, context);
+            let scope = &mut v8::ContextScope::new(handle_scope, context);
+
+            // Load js file
+            let code = include_str!("../js/out/index.mjs");
+            let source = v8::String::new(scope, code).unwrap();
+            let origin = create_origin(scope, "index.js", true);
+            let source = v8::script_compiler::Source::new(source, Some(&origin));
+            let module = v8::script_compiler::compile_module(scope, source).unwrap();
+
+            // Instantiate module
+            module.instantiate_module(scope, module_callback).unwrap();
+            module.evaluate(scope).unwrap();
+
+            let key = v8::String::new(scope, "default").unwrap();
+            let obj = module
+                .get_module_namespace()
+                .to_object(scope)
+                .unwrap()
+                .get(scope, key.into())
+                .unwrap();
+            let key = v8::String::new(scope, FUNC_ID).unwrap().into();
+            context.global(scope).set(scope, key, obj);
+        }
+    }
+    (RefCell::new(isolate), global_context)
 }
 
-impl Converter {
-    /// Initialization: JIT compilation and function object registration
-    pub fn new() -> Self {
-        if !*INITIALIZED.lock().unwrap() {
-            let platform = v8::new_default_platform(0, false).make_shared();
-            v8::V8::initialize_platform(platform);
-            v8::V8::initialize();
-            *INITIALIZED.lock().unwrap() = true;
-        }
+/// Convert a math string to Svg
+pub fn convert_to_svg(latex: impl AsRef<str>) -> Result<String> {
+    thread_local! {
+        pub static ISOLATE_CONTEXT: (RefCell<OwnedIsolate>, Global<Context>) = initialize();
+    };
+    ISOLATE_CONTEXT.with(
+        |(isolate, context): &(RefCell<OwnedIsolate>, Global<Context>)| {
+            let isolate: &mut OwnedIsolate = &mut isolate.borrow_mut();
+            let handle_scope = &mut v8::HandleScope::new(isolate);
+            let context = v8::Local::new(handle_scope, context.clone());
+            let scope = &mut v8::ContextScope::new(handle_scope, context);
+            let scope = &mut v8::TryCatch::new(scope);
 
-        let mut isolate = v8::Isolate::new(Default::default());
-        let global_context;
-        {
-            let handle_scope = &mut v8::HandleScope::new(&mut isolate);
-            let context = v8::Context::new(handle_scope);
-            global_context = Global::new(handle_scope, context);
-            {
-                // JIT compilation and function object registration
-
-                // Generate scope
-                let context = v8::Local::new(handle_scope, context);
-                let scope = &mut v8::ContextScope::new(handle_scope, context);
-
-                // Load js file
-                let code = include_str!("../js/out/index.mjs");
-                let source = v8::String::new(scope, code).unwrap();
-                let origin = create_origin(scope, "index.js", true);
-                let source = v8::script_compiler::Source::new(source, Some(&origin));
-                let module = v8::script_compiler::compile_module(scope, source).unwrap();
-
-                // Instantiate module
-                module.instantiate_module(scope, module_callback).unwrap();
-                module.evaluate(scope).unwrap();
-
-                let key = v8::String::new(scope, "default").unwrap();
-                let obj = module
-                    .get_module_namespace()
-                    .to_object(scope)
-                    .unwrap()
-                    .get(scope, key.into())
-                    .unwrap();
-                let key = v8::String::new(scope, FUNC_ID).unwrap().into();
-                context.global(scope).set(scope, key, obj);
+            let key = v8::String::new(scope, FUNC_ID).unwrap().into();
+            let obj = context.global(scope).get(scope, key).unwrap();
+            let func = v8::Local::<v8::Function>::try_from(obj).unwrap();
+            let args = [v8::String::new(scope, latex.as_ref()).unwrap().into()];
+            if let Some(result) = func.call(scope, obj, &args) {
+                Ok(result.to_rust_string_lossy(scope))
+            } else {
+                let message = 'msg: {
+                    let key = v8::String::new(scope, "message").unwrap();
+                    let Some(exception) = scope.exception() else {
+                        return Err(Error::UnknownError);
+                    };
+                    let exception = exception.to_object(scope).unwrap();
+                    let Some(message) = exception.get(scope, key.into()) else {
+                        // If the error object does not contain a `message` member, it will be directly converted to a string.
+                        break 'msg exception.to_rust_string_lossy(scope);
+                    };
+                    message.to_rust_string_lossy(scope)
+                };
+                Err(Error::V8ExceptionThrown(message))
             }
-        }
-        Self {
-            isolate,
-            context: global_context,
-        }
-    }
-
-    /// Convert a math string to Svg
-    pub fn convert_to_svg(&mut self, latex: impl AsRef<str>) -> Result<String> {
-        let handle_scope = &mut v8::HandleScope::new(&mut self.isolate);
-        let context = v8::Local::new(handle_scope, self.context.clone());
-        let scope = &mut v8::ContextScope::new(handle_scope, context);
-        let scope = &mut v8::TryCatch::new(scope);
-
-        let key = v8::String::new(scope, FUNC_ID).unwrap().into();
-        let obj = context.global(scope).get(scope, key).unwrap();
-        let func = v8::Local::<v8::Function>::try_from(obj).unwrap();
-        let args = [v8::String::new(scope, latex.as_ref()).unwrap().into()];
-        if let Some(result) = func.call(scope, obj, &args) {
-            Ok(result.to_rust_string_lossy(scope))
-        } else {
-            let message = 'msg: {
-                let key = v8::String::new(scope, "message").unwrap();
-                let Some(exception) = scope.exception() else {
-                    return Err(Error::UnknownError);
-                };
-                let exception = exception.to_object(scope).unwrap();
-                let Some(message) = exception.get(scope, key.into()) else {
-                    // If the error object does not contain a `message` member, it will be directly converted to a string.
-                    break 'msg exception.to_rust_string_lossy(scope);
-                };
-                message.to_rust_string_lossy(scope)
-            };
-            Err(Error::V8ExceptionThrown(message))
-        }
-    }
+        },
+    )
 }
